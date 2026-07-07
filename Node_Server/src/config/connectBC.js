fcb7fbfa6efca30c0a15ffcf2445cb655300ddff
Ivan Epou <ivanepou92@gmail.com>
2026-07-07 11:56:12 +0300

    Add connectBC configuration for API authentication

---
+++ Node_Server/src/config/connectBC.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const password = process.env.BC_PASSWORD;
+const username = process.env.BC_USERNAME;
+
+const base64Credentials = Buffer.from(`${username}:${password}`).toString('base64');
+
+export default connectBc = {
+    headers: {
+        Authorization: `Basic ${base64Credentials}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+    },
+};
